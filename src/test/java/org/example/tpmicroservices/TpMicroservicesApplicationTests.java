e376e81f2de9ddabe626ee0a280f2c49cec02e71
Tacinaa <tasnimeyahiaoui59@gmail.com>
2025-02-04 09:34:16 +0100

    init project

---
+++ src/test/java/org/example/tpmicroservices/TpMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tpmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
